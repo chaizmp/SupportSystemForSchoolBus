085ed7805c679b801345c8e8240063eb0a7d04f8
chaiz poonperm <chaiz poonperm>
2016-08-28 23:38:24 +0700

    Initialization project with Mysql Database and Spring Boot (Localhost)

---
+++ src/main/java/Project/Connection/ConnectionConfiguration.java
@@ -0,0 +1,38 @@
+package Project.Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Created by User on 14/10/2558.
+ */
+
+/**
+ * Created by User on 12/10/2558.
+ */
+public class ConnectionConfiguration {
+
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/StudentInformation", "root","password");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void closeConnection(ResultSet rs, Statement st, Connection conn) {
+        try {
+            rs.close();
+            st.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Project/SchoolBusSupportSystemApplication.java
@@ -0,0 +1,43 @@
+package Project;
+
+import Project.Connection.ConnectionConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class SchoolBusSupportSystemApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SchoolBusSupportSystemApplication.class, args);
+		Connection connection = null;
+		try
+		{
+			connection = ConnectionConfiguration.getConnection();
+			if (connection != null)
+			{
+				System.out.println("Connection established");
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (connection != null)
+			{
+				try
+				{
+					connection.close();
+				} catch (SQLException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
